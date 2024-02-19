c861e73841184c97689cce259883ea26c306db81
vini1cas <155457470+vini1cas@users.noreply.github.com>
2024-02-19 22:09:03 +0000

    Import libraries and add function to retrieve sales data from the user

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+#sales = SHEET.worksheet('sales')
+#data =sales.get_all_values()
+
+#print(data)
+
+def get_sales_data():
+    """
+    Function to get sales data input from the user
+    """
+
+    print("Please enter sales date from the last market day.\n Data should be six numbers separated by commas\n Example 10, 20, 30, 40 , 50")
+
+    data_str = input("Please enter your data here:\n")
+    print(f'The data provided is {data_str}')
+
+    sales_data = data_str.split(",")
+
+get_sales_data()
+
+
+
